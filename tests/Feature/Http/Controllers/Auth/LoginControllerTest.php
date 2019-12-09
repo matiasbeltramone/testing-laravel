f8f43ca8f3fe226e47c704c68967ba1591cc2ae0
Matias Beltramone <mgbeltramone@gmail.com>
2019-12-09 09:34:01 -0300

    Tests Configuration

---
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function login_displays_the_login_form()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+
+    /** @test */
+    public function login_displays_validation_errors()
+    {
+        $response = $this->post(route('login'), []);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function login_authenticates_and_redirects_user()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function register_creates_and_authenticates_a_user()
+    {
+        $name = $this->faker->name;
+        $email = $this->faker->safeEmail;
+        $password = $this->faker->password(8);
+
+        $response = $this->post('register', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password,
+        ]);
+
+        $response->assertRedirect(route('home'));
+
+        $this->assertDatabaseHas('users', [
+            'name' => $name,
+            'email' => $email
+        ]);
+    }
+}
